3f6b0c5c1c8d9d52988127fb53653116a2e672ea
DmitriyKolesnikM8O <143881957+DmitriyKolesnikM8O@users.noreply.github.com>
2023-10-19 16:20:58 +0300

    laba_9_1

--- laba9.c
+++ laba9.c
@@ -2,19 +2,34 @@
 #include <math.h>
 
 
-int sgn(int val)
-{
-   return (val > 0) ? (1) : ((val < 0) ? (-1) : (0));
+int define_sgn(int val) {
+    if(val > 0){
+        return 1;
+    }
+    else if(val < 0){
+        return -1;
+    }
+    return 0;
+   
 }
+
+int Mod(int x, int div) {
+    int m = x % div;
+    if(m < 0) {
+        m = (div < 0) ? m - div: m + div;
+    }
+    return m;
+}
+
 
 int main() {
     int i = -11, j = -6, l = -5;
     int k;
 
     for (k = 0; k < 50; k++) {
-        int new_i = (i + j + l) * (k + l) % 25 - i * j * l * (k + 2) % 10 + 10;
-        int new_j = fmin((i + j + l) * (k + 3) % 25, i * j * l * (k + 4) % 25) + 10;
-        int new_l = 2 * sgn(l) * abs((i + j + l) * (k + 5) % 10 - i * j * l * (k + 6) % 25);
+        int new_i = Mod((i + j + l) * (k + l), 25) - Mod(i * j * l * (k + 2), 10) + 10;
+        int new_j = fmin(Mod((i + j + l) * (k + 3), 25), Mod(i * j * l * (k + 4), 25)) + 10;
+        int new_l = 2 * define_sgn(l) * abs(Mod((i + j + l) * (k + 5), 10) - Mod(i * j * l * (k + 6), 25));
         
         i = new_i;
         j = new_j;
