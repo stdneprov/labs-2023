3d9dd1d9f34ad59720386c5a5f8f70b6906fd8ef
ArtemDelGray <143866267+ArtemDelGray@users.noreply.github.com>
2023-10-20 10:53:55 +0300

    New_Lab9

--- lab9/lab9.c
+++ lab9/lab9.c
@@ -7,47 +7,65 @@
 int sign(int x) {
     if (x < 0) {
         return -1;
-    }
-    else if (x > 0) {
+    } else if (x > 0) {
         return 1;
     } else {
         return 0;
     }
 }
-int min(int x, int y) {
-    if (x > y) {
-        return y;
-    }
-    else{
-        return x;
-    }
-}
-int max(int x, int y) {
-    if (x > y) {
-        return x;
-    }
-    return y;
+//int min(int x, int y) {
+//    if (x > y) {
+//        return y;
+//    } else {
+//        return x;
+//    }
+//}
+//int max(int x, int y) {
+//    if (x > y) {
+//        return x;
+//    }
+//    return y;
+//}
+int max(int a, int b) {
+    int m;
+    m = (a > b) ? a : b;
+    return m;
 }
 
+int min(int a, int b) {
+    int m;
+    m = (a < b) ? a : b;
+    return m;
+}
+
+int mod(int a, int b) {
+  int m = a % b;
+  if (m < 0) {
+    m = (b < 0) ? m - b : m + b;
+  }
+  return m;
+}
 int main() {
     int i0 = -8, j0 = -5, l0 = 12;
     int i = -8, j = -5, l = 12;
     int d = 0;
     for (int k = 1; k <= 50; k++) {
-        i = ((i0 * i0 / (abs(j0 - l0) + k + 1)) - (j0 * j0 / (abs(i0 - l0) + k + 1))) % 30;
+        i = mod(((i0 * i0 / (abs(j0 - l0) + k + 1)) - (j0 * j0 / (abs(i0 - l0) + k + 1))), 30);
+//        i = (((i0 * i0 / (abs(j0 - l0) + k + 1)) - (j0 * j0 / (abs(i0 - l0) + k + 1))) % 30);
         j = sign(l0) * min(i0, j0) - sign(j0) * max(i0,l0) + k;
-        l = ((i0 - j0) * (j0 - l0) * (l0 - i0)) % 20;
+        l = mod(((i0 - j0) * (j0 - l0) * (l0 - i0)), 20);
+//        l = (((i0 - j0) * (j0 - l0) * (l0 - i0)) % 20);
         l0 = l;
         i0 = i;
         j0 = j;
-        if ((i0 >= (-10)) && (i0 <= 0) && (j0 >= i0 + 10) && (j0 <= 10 - i0)){
-            printf("Цель попадёт в зону через %d шагов \n", k);
+        if ((i0 > (-10)) && (i0 < 0) && (j0 > i0 + 10) && (j0 < 10 - i0)) {
+            printf("YES %d \n", k);
             d = 1;
             break;
         }
     }
     if (d == 0) {
-        printf("к цели не пришли");
+        printf("NO\n");
     }
     printf("Конечная точка: %d %d \n", i0, j0);
     printf("Конечное значение динамического параметра: %d \n", l0);
