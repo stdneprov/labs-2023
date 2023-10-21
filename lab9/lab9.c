357b2b3657c7a8adf085e7c649972c6af7fd4390
ArtemDelGray <143866267+ArtemDelGray@users.noreply.github.com>
2023-10-21 13:03:52 +0300

    Codestylelab9

--- lab9/lab9.c
+++ lab9/lab9.c
@@ -4,7 +4,7 @@
 /*
 И так тоже могу
 */
-int sign(int x) {
+int Sign(int x) {
     if (x < 0) {
         return -1;
     } else if (x > 0) {
@@ -13,32 +13,32 @@ int sign(int x) {
         return 0;
     }
 }
-//int min(int x, int y) {
+//int Min(int x, int y) {
 //    if (x > y) {
 //        return y;
 //    } else {
 //        return x;
 //    }
 //}
-//int max(int x, int y) {
+//int Max(int x, int y) {
 //    if (x > y) {
 //        return x;
 //    }
 //    return y;
 //}
-int max(int a, int b) {
+int Max(int a, int b) {
     int m;
     m = (a > b) ? a : b;
     return m;
 }
 
-int min(int a, int b) {
+int Min(int a, int b) {
     int m;
     m = (a < b) ? a : b;
     return m;
 }
 
-int mod(int a, int b) {
+int Mod(int a, int b) {
   int m = a % b;
   if (m < 0) {
     m = (b < 0) ? m - b : m + b;
@@ -50,10 +50,10 @@ int main() {
     int i = -8, j = -5, l = 12;
     int d = 0;
     for (int k = 1; k <= 50; k++) {
-        i = mod(((i0 * i0 / (abs(j0 - l0) + k + 1)) - (j0 * j0 / (abs(i0 - l0) + k + 1))), 30);
+        i = Mod(((i0 * i0 / (abs(j0 - l0) + k + 1)) - (j0 * j0 / (abs(i0 - l0) + k + 1))), 30);
 //        i = (((i0 * i0 / (abs(j0 - l0) + k + 1)) - (j0 * j0 / (abs(i0 - l0) + k + 1))) % 30);
-        j = sign(l0) * min(i0, j0) - sign(j0) * max(i0,l0) + k;
-        l = mod(((i0 - j0) * (j0 - l0) * (l0 - i0)), 20);
+        j = Sign(l0) * Min(i0, j0) - Sign(j0) * Max(i0,l0) + k;
+        l = Mod(((i0 - j0) * (j0 - l0) * (l0 - i0)), 20);
 //        l = (((i0 - j0) * (j0 - l0) * (l0 - i0)) % 20);
         l0 = l;
         i0 = i;
