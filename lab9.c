711409314928490105213d461f761ea617b9fa3d
marinda6666 <143881795+marinda6666@users.noreply.github.com>
2023-10-21 13:36:53 +0300

    Update lab9.c

--- lab9.c
+++ lab9.c
@@ -2,24 +2,25 @@
 #include <stdbool.h>
 #include <math.h>
 
-int mod(int a, int b){
+int Mod(int a, int b){
     return (a % b + b) % b;
 }
 int main() {
     int i = 6, j = 27, l = -15;
     for (int k = 0; k < 51; ++k){
-        int newI = mod((pow(i, 3) - pow(j, 3) + pow(l, 3) - k), 20);
-        int newJ = mod(fmin(fmin(i * j * j - k, i * i * l - k), j * l * l - k), 30);
-        int newL = mod(fmax(fmin(i * j * j - k, i * i * l - k), j * l * l - k), 30);
+        int newI = Mod((pow(i, 3) - pow(j, 3) + pow(l, 3) - k), 20);
+        int newJ = Mod(fmin(fmin(i * j * j - k, i * i * l - k), j * l * l - k), 30);
+        int newL = Mod(fmax(fmin(i * j * j - k, i * i * l - k), j * l * l - k), 30);
         i = newI;
         j = newJ;
         l = newL;
         if (((pow(i + 10, 2) + pow(j + 10, 2)) <= 100) && ((pow(i + 20, 2) + pow(j + 20, 2)) <= 100)){
-            printf("Шаг: %d, Время: %d, Координаты: (%d, %d), l: %d\n", k, k + 1, i, j, l);   
+            printf("YES");
+            printf("%d", "%d", "%d", "%d", k, i, j, l);   
             break;         
         }
         else if (k == 50){
-            printf("Промах!");
+            printf("NO");
         }
     }
     return 0;
