21976dbf208dead9a4536353f521f0f519955958
marinda6666 <143881795+marinda6666@users.noreply.github.com>
2023-10-21 13:12:51 +0300

    Update lab9.c

--- lab9.c
+++ lab9.c
@@ -8,9 +8,12 @@ int mod(int a, int b){
 int main() {
     int i = 6, j = 27, l = -15;
     for (int k = 0; k < 51; ++k){
-        int new_i = mod((pow(i, 3) - pow(j, 3) + pow(l, 3) - k), 20);
-        int new_j = mod(fmin(fmin(i * j * j - k, i * i * l - k), j * l * l - k), 30);
-        int new_l = mod(fmax(fmin(i * j * j - k, i * i * l - k), j * l * l - k), 30);
+        int newI = mod((pow(i, 3) - pow(j, 3) + pow(l, 3) - k), 20);
+        int newJ = mod(fmin(fmin(i * j * j - k, i * i * l - k), j * l * l - k), 30);
+        int newL = mod(fmax(fmin(i * j * j - k, i * i * l - k), j * l * l - k), 30);
+        i = newI;
+        j = newJ;
+        l = newL;
         if (((pow(i+10, 2) + pow(j+10, 2)) <= 100) && ((pow(i+20, 2) + pow(j+20, 2)) <= 100)){
             printf("Шаг: %d, Время: %d, Координаты: (%d, %d), l: %d\n", k, k + 1, i, j, l);   
             break;         
@@ -19,6 +22,5 @@ int main() {
             printf("Промах!");
         }
     }
-    
     return 0;
 }
