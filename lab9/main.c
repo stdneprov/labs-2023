8af84a899515cea776bb5272983b7caecf3e300c
nPaBwaYT <surge.anton@gmail.com>
2023-10-21 12:35:28 +0300

    сделал функцию  Mod

--- lab9/main.c
+++ lab9/main.c
@@ -4,6 +4,16 @@
 #define START_POS_Y -1
 #define START_MOTION_PARAM_L -9
 
+// void BuildGraph(FILE *gnupipe){
+//     fprintf(gnupipe, "reset\n");
+//     fprintf(gnupipe, "set title \"Graph\"\n");
+//     fprintf(gnupipe, "set parametric\n");
+//     fprintf(gnupipe, "set size square\n");
+//     fprintf(gnupipe, "set xrange [-10:50]\n");
+//     fprintf(gnupipe, "set yrange [-10:50]\n");
+//     fprintf(gnupipe, "plot 'data.txt'with linespoints, [t=0:2*pi] sin(t)*10+10,cos(t)*10+10, [t=0:2*pi] sin(t)*5+10,cos(t)*5+10\n");
+// }
+
 long long Max(long long a, long long b){
     if (a > b){
         return a;
@@ -21,15 +31,28 @@ long long Min(long long a, long long b){
     }
 }
 
-// void BuildGraph(FILE *gnupipe){
-//     fprintf(gnupipe, "reset\n");
-//     fprintf(gnupipe, "set title \"Graph\"\n");
-//     fprintf(gnupipe, "set parametric\n");
-//     fprintf(gnupipe, "set size square\n");
-//     fprintf(gnupipe, "set xrange [-40:40]\n");
-//     fprintf(gnupipe, "set yrange [-40:40]\n");
-//     fprintf(gnupipe, "plot 'data.txt'with linespoints, [t=0:2*pi] sin(t)*10+10,cos(t)*10+10, [t=0:2*pi] sin(t)*5+10,cos(t)*5+10\n");
-// }
+long long Mod(long long a, long long b){
+    if (b == 0){
+        return 1 / 0; 
+    }
+    
+    if (b < 0){
+        a = -a;
+        b = -b;
+    }
+
+    if (a >= 0){
+        while (a >= b){
+            a -= b;
+        }
+    } else {
+        while (a < 0){
+            a += b;
+        }
+    }
+
+    return a;
+}
 
 int Sign(long long a){
     if (a >= 0) {
@@ -38,8 +61,6 @@ int Sign(long long a){
         return -1;
     }
 }
-
-
 
 int main(){
     long long i = START_POS_X, j = START_POS_Y, l = START_MOTION_PARAM_L;    
@@ -58,9 +79,9 @@ int main(){
         prevJ = j;
         prevL = l;
         
-        i = Max(prevJ - k, prevL - k) % 30 + Max(prevI + prevL, prevJ + k) % 20;
-        j = (abs(prevI - prevL) * Sign(prevJ + k) + abs(prevI - k) * (prevJ + k)) % 20;
-        l = (prevI + k) * (prevJ - k) * (prevL + k) % 25;
+        i = Mod(Max(prevJ - k, prevL - k), 30) + Mod(Max(prevI + prevL, prevJ + k), 20);
+        j = Mod(abs(prevI - prevL) * Sign(prevJ + k) + abs(prevI - k) * (prevJ + k), 20);
+        l = Mod((prevI + k) * (prevJ - k) * (prevL + k), 25);
 
         // fprintf(data, "%lld %lld\n", i, j);
 
