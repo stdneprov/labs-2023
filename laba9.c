a328ce9fd045578f96c12ac94fbf0aff01591973
DmitriyKolesnikM8O <143881957+DmitriyKolesnikM8O@users.noreply.github.com>
2023-10-21 13:30:48 +0300

    Laba_9_3

--- laba9.c
+++ laba9.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 
-int defineSgn(int val) {
+int DefineSgn(int val) {
     if(val > 0){
         return 1;
     }
@@ -29,20 +29,20 @@ int main() {
     for (k = 0; k < 50; k++) {
         int newI = Mod((i + j + l) * (k + l), 25) - Mod(i * j * l * (k + 2), 10) + 10;
         int newJ = fmin(Mod((i + j + l) * (k + 3), 25), Mod(i * j * l * (k + 4), 25)) + 10;
-        int newL = 2 * defineSgn(l) * abs(Mod((i + j + l) * (k + 5), 10) - Mod(i * j * l * (k + 6), 25));
+        int newL = 2 * DefineSgn(l) * abs(Mod((i + j + l) * (k + 5), 10) - Mod(i * j * l * (k + 6), 25));
         
         i = newI;
         j = newJ;
         l = newL;
 
         if (i >= 5 && i <= 15 && j >= -15 && j <= -5) {
-            printf("Попадание на шаге %d. Время: %d, Координаты: (%d, %d), l: %d\n", k, k + 1, i, j, l);
+            printf("YES - %d - %d - (%d, %d), l: %d\n", k, k + 1, i, j, l);
             break;
         }
     }
 
     if (k == 50) {
-        printf("Промах. Время окончания: %d, Координаты: (%d, %d), l: %d\n", k, i, j, l);
+        printf("NO - %d - (%d, %d), l: %d\n", k, i, j, l);
     }
 
     return 0;
