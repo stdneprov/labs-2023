48db969c0833740f7376a82efb399be171e2496f
Max Giga <gigamaximwachau@gmail.com>
2023-11-06 14:15:20 +0300

    make code cleaner

--- lab11/lab11.c
+++ lab11/lab11.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+bool isSeparator(char c) {
+    return c == ' ' || c == '\t' || c == '\n' || c == ',' || c == '.' ||
+           c == EOF;
+}
+
 int main(void) {
     char c;
     bool is_lower = true;
@@ -9,22 +14,17 @@ int main(void) {
     int res = 0;
     while (true) {
         c = getchar();
-        if (isspace(c) || c == EOF) {
-            if (is_lower && is_word) {
-                res++;
-            }
+        if (isSeparator(c)) {
+            res += is_lower && is_word;
             is_lower = true;
             is_word = false;
-            if (c == EOF) {
-                break;
-            }
-            continue;
-        }
-        if (islower(c)) {
+        } else if (islower(c))
             is_word = true;
-        } else {
+        else
             is_lower = false;
-        }
+
+        if (c == EOF)
+            break;
     };
     printf("%d \n", res);
     return 0;
