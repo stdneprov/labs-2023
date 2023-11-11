2362d8b1345ffc5caadc752e54848baf2378d042
alim <djobeyka061@gmail.com>
2023-11-11 15:30:08 +0300

    Переделал лаб11

--- lab11/lab11.c
+++ lab11/lab11.c
@@ -13,8 +13,10 @@ int main() {
     bool isWasDigit = 0;
     bool isWordStarted = 0;
     bool isWordIsNumber = 1;
+    bool leaderNul = 1;
     long long buffer = 0;
     int sign = 1;
+    int countNul = 0;
 
     while (1) {
         int c = getchar();
@@ -30,18 +32,39 @@ int main() {
 
             if (IsDigit(c)) {
                 buffer = buffer * 10 + (c - '0');
+                if (c != '0') {
+                    leaderNul = 0;
+                }
+                if (leaderNul) {
+                    countNul += 1;
+                }
             }
         } else {
             if (isWordStarted && isWasDigit && isWordIsNumber && sign == 1) {
+                if (buffer == 0) {
+                    countNul -= 1;
+                }
+                for (int i = 0; i < countNul; i++) {
+                    putchar('0');
+                }
                 printf("0%lld\n", buffer);
             } else if(isWordStarted && isWasDigit && isWordIsNumber && sign == -1) {
-                printf("-0%lld\n", buffer);
+                if (buffer == 0) {
+                    countNul -= 1;
+                }
+                putchar('-');
+                for (int i = 0; i < countNul; i++) {
+                    putchar('0');
+                }
+                printf("0%lld\n", buffer);
             }
             isWasDigit = 0;
             isWordIsNumber = 1;
             isWordStarted = 0;
             buffer = 0;
             sign = 1;
+            leaderNul = 1;
+            countNul = 0;
         }
 
 
