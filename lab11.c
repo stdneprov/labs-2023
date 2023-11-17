cd54ab22a91a9e64704cbccbb561dae77097e85b
evaki53 <143840299+evaki53@users.noreply.github.com>
2023-11-17 22:12:58 +0300

    zaitova lab11

---
+++ lab11.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int IsSeparator(char c){
+    return (c == ' ') || (c == EOF) || (c == '\n');
+}
+int IsDigitForBCD(char c){
+    return (c == '0') || (c == '1');
+}
+
+int main(){
+    long long numberCounter = 0;
+    int isNumberStarted = 0;
+    int countDigitBeforeCurrentEl = 0;
+    long long buffer = 0;
+    int sepBefore = 1;
+    while(1){
+        int c = getchar();
+        if ((IsDigitForBCD(c)) && (isNumberStarted == 0) && (sepBefore == 1)){
+            isNumberStarted = 1;
+            countDigitBeforeCurrentEl += 1;
+            buffer = buffer * 2 + (c - '0');
+            printf("%lld %lld\n", numberCounter,buffer);
+        }else if ((IsDigitForBCD(c) && (isNumberStarted == 1) && (countDigitBeforeCurrentEl < 4))){
+            countDigitBeforeCurrentEl += 1;
+            buffer = buffer * 2 + (c - '0');
+            printf("%lld %lld %d\n", numberCounter,buffer, countDigitBeforeCurrentEl);
+        }else if (countDigitBeforeCurrentEl == 4){
+            if (c == EOF){
+                printf("f");
+            }
+            if (IsDigitForBCD(c) && (buffer <= 9)){
+                countDigitBeforeCurrentEl = 0;
+                countDigitBeforeCurrentEl += 1;
+                buffer = 0;
+                buffer = buffer * 2 + (c - '0');
+                printf("%lld %lld %d\n", numberCounter, buffer, countDigitBeforeCurrentEl);
+            }else if (IsSeparator(c) && (buffer <= 9)){
+                countDigitBeforeCurrentEl = 0;
+                isNumberStarted = 0;
+                buffer = 0;
+                numberCounter += 1;
+                printf("%lld %lld %d\n", numberCounter,buffer, countDigitBeforeCurrentEl);
+            }else{
+                countDigitBeforeCurrentEl = 0;
+                isNumberStarted = 0;
+                buffer = 0;
+            }
+        }else{
+            countDigitBeforeCurrentEl = 0;
+            isNumberStarted = 0;
+            buffer = 0;
+        }
+        sepBefore = IsSeparator(c);
+        if (c == EOF){
+            break;
+        }
+        }
+    if (numberCounter > 0){
+        printf("%lld\n",numberCounter);
+    }else{
+        printf("not found\n");
+    }
+    return 0;
+}
