3f532a337da5ecde48aa87d690b11a534a86994c
Adam Arselgov <adamarselgov4@gmail.com>
2023-11-25 18:27:22 +0300

    Адам Арсельгов лабораторная 11

--- Лаба 11/laba11.c
+++ Лаба 11/laba11.c
@@ -2,30 +2,24 @@
 #include <ctype.h>
 
 int count_nouns() {
-    int count = 0;    // Переменная для подсчета количества существительных
-    int isWord = 0;   // Флаг, указывающий, что мы считываем слово
-
-    int currentChar = getchar();   // Считываем символы с помощью функции getchar()
-
-    while (currentChar != EOF && currentChar != '\n') {   // Пока не достигнут конец файла или символ новой строки
-        if (isalpha(currentChar)) {   // Проверяем, является ли символ буквой
-            isWord = 1;   // Устанавливаем флаг, что мы считываем слово
-        } else if (isWord) {   // Если прочитали слово
-            count++;   // Увеличиваем счетчик на 1
-            isWord = 0;   // Сбрасываем флаг слова
+    int count = 0;    
+    int isWord = 0;   
+    int currentChar = getchar();   
+    while (currentChar != EOF && currentChar != '\n') {  
+        if (isalpha(currentChar)) {   
+            isWord = 1;  
+        } else if (isWord) {  
+            count++;  
+            isWord = 0;   
         }
-        
-        currentChar = getchar();   // Считываем следующий символ
+        currentChar = getchar();   
     }
-
     return count;
 }
 
 int main() {
     printf("Введите текст на немецком языке:\n");
-
     int noun_count = count_nouns();
     printf("Количество существительных в тексте: %d\n", noun_count);
-
     return 0;
 }
