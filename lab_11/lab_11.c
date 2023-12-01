da5ec4e3e3252b7b0d5b511ec1df16ce091dad5e
sofasol <sofiasolodova288@gmail.com>
2023-12-01 21:18:48 +0300

    lab_11

---
+++ lab_11/lab_11.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdbool.h>
+int IsSeparator(char c) {
+    return (c == ' ') || (c == ',') || (c == '\n') || (c == EOF);
+}
+
+int main(){
+    int WordCounter = 0;
+    bool CommentStarted = 0;
+    bool WordStarted = 0;
+    int PredSymbol = 0;
+
+    while (1) {
+        int c = getchar();
+        if (!IsSeparator(c)) {
+            if (c == '{' || (c == '*' && PredSymbol == '(')){
+                CommentStarted = 1;
+                WordStarted = 0;
+            }
+            else if (c == '}' || c == ')' && PredSymbol == '*'){
+                CommentStarted = 0;
+                if (WordStarted){
+                    WordStarted = 0;
+                    WordCounter += 1;
+                }
+            }
+            else{
+                WordStarted = 1;
+
+            }
+
+        } else {
+            if (WordStarted && CommentStarted){
+                WordCounter += 1;
+                WordStarted = 0;
+            }
+            WordStarted = 0;
+        }
+        PredSymbol = c;
+        if (c == EOF) {
+            break;
+        }
+    }
+
+    printf("%d", WordCounter);
+
+}
