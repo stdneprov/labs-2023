bffe555c111b92c7233701b5a319b869464cf97b
nPaBwaYT <surge.anton@gmail.com>
2023-12-02 11:51:05 +0300

    переписал вообще всю логику матриц, теперь они отдельный тип

--- lab15/main.c
+++ lab15/main.c
@@ -2,70 +2,79 @@
 #include <stdlib.h>
 const int maxn = 400;
 
-// я не понял как передавать в функции в качестве параметров массивы нефиксированных размеров
-// поэтому функции работают через указатель на первый элемент матрицы
 
-int ColumnSumm(int *ptr, int x, int size) { // сумма всех элементов x-го столбца 
-    int sum = 0;
-    for (int y = 0; y < size; ++y) {
-        sum += *(ptr + y * size + x);
+typedef struct
+{
+    int *ptr;
+    int size;
+} matrix;
+
+matrix MatrixIn(int *ptr) { // ввод матрицы
+    
+    int *writer = ptr;
+    int n = 0;
+    while (scanf("%d", writer++) != EOF) {
+
+        if (n*n < (writer - ptr)) {
+            ++n;
+        }
     }
-    return sum;
+
+    matrix res = {ptr, n};
+    return res;
 }
 
-void PrintMatrix(int *ptr, int size) { // вывод матрицы
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            printf("%d ", *(ptr + i * size + j));
+int GetMatrixElem(matrix mx, int i, int j) { // удобное получение элемента в 0-индексации
+    return *(mx.ptr + i * mx.size + j);
+}
+
+void PutMatrixElem(matrix mx, int i, int j, int el) { // удобная запись элемента в 0-индексации
+    int *where = mx.ptr + i * mx.size + j;
+    *where = el;
+}
+
+void PrintMatrix(matrix mx) { // вывод матрицы
+    for (int i = 0; i < mx.size; ++i) {
+        for (int j = 0; j < mx.size; ++j) {
+            printf("%d ", GetMatrixElem(mx, i, j));
         }
         putchar('\n');
     }
 }
 
+int ColumnSumm(matrix mx, int x) { // сумма всех элементов x-го столбца 
+    int sum = 0;
+    for (int y = 0; y < mx.size; ++y) {
+        sum += GetMatrixElem(mx, y, x);
+    }
+    return sum;
+}
+
 int main() {
 
-    int elements[maxn];
+    int elements[maxn]; 
+    matrix m = MatrixIn((int *)&elements);
+
     int min = 1e5;
-
-    int size = 0; // количество элементов
-    int n = 0; // порядок матрицы
-    
-    while (scanf("%d", &elements[size]) != EOF) {
-        if (elements[size] < min) {
-            min = elements[size];
-        }
-        ++size;
-        if (n * n < size) {
-            ++n;
-        }
-    }
-
-    int matrix[n][n];
-    int *matrixPtr = &matrix[0][0];
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            matrix[i][j] = elements[i * n + j];
-        }
-    }
-
-    int modifiedMatrix[n][n];
-    int *modMatrixPtr = &modifiedMatrix[0][0];
-
-    for (int x = 0; x < n; ++x) {
-        int cs = ColumnSumm(matrixPtr, x, n);
-
-        for (int y = 0; y < n; ++y) {
-
-            if (matrix[y][x] == min) {
-                modifiedMatrix[y][x] = cs;
-            } else {
-                modifiedMatrix[y][x] = matrix[y][x];
+    for (int i = 0; i < m.size; ++i) {
+        for (int j = 0; j < m.size; ++j) {
+            if (GetMatrixElem(m, i, j) < min) {
+                min = GetMatrixElem(m, i, j);
             }
         }
     }
 
-    PrintMatrix(modMatrixPtr, n);
+    int cs;
+    for (int x = 0; x < m.size; ++x) {
+        cs = ColumnSumm(m, x);
+        for (int y = 0; y < m.size; ++y) {
+            if (GetMatrixElem(m, y, x) == min) {
+                PutMatrixElem(m, y, x, cs);
+            }
+        }
+    }
+
+    PrintMatrix(m);
     
     return 0;
 }
