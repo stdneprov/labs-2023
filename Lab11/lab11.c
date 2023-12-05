c66d2b9ffb433cbee31106ec924f2fdce2a7f838
Shah29py <gebeevs@gmail.com>
2023-12-05 21:20:55 +0300

    lab11

---
+++ Lab11/lab11.c
@@ -0,0 +1,18 @@
+#include  <stdio.h>
+
+int main() {
+    char c;
+    int first = 1; 
+    while ((c = getchar()) != EOF) {
+        if (first) {
+            putchar(' ');
+            first = 0;
+        }
+        else if (c == '\n') {
+           first = 1;
+        }
+        putchar(c);
+    }
+    putchar(' ');
+    return 0;
+}
