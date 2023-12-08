14631b1855f2fec98dd7f0b1dbc38ecdb163037c
Max Giga <gigamaximwachau@gmail.com>
2023-12-08 16:11:33 +0300

    allow use of non-square matrices

--- lab14/lab14.c
+++ lab14/lab14.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 
-void PrintMatrix(int n, int *matrix) {
-    int x0 = 0, x1 = n - 1, y0 = 0, y1 = n - 1;
-    int i = n * n;
-    while (i > 0) {
+void PrintMatrix(int n, int m, int *matrix) {
+    int x0 = 0, x1 = n - 1, y0 = 0, y1 = m - 1;
+    while (x0 <= x1 && y0 <= y1) {
         for (int x = x0; x <= x1; x++) {
             printf("%d ", matrix[y0 * n + x]);
-            i--;
         }
         y0++;
         for (int y = y0; y <= y1; y++) {
             printf("%d ", matrix[y * n + x1]);
-            i--;
         }
         x1--;
-        for (int x = x1; x >= x0; x--) {
-            printf("%d ", matrix[y1 * n + x]);
-            i--;
+        if (y0 <= y1) {
+            for (int x = x1; x >= x0; x--) {
+                printf("%d ", matrix[y1 * n + x]);
+            }
+            y1--;
         }
-        y1--;
-        for (int y = y1; y >= y0; y--) {
-            printf("%d ", matrix[y * n + x0]);
-            i--;
+        if (x0 <= x1) {
+            for (int y = y1; y >= y0; y--) {
+                printf("%d ", matrix[y * n + x0]);
+            }
+            x0++;
         }
-        x0++;
     }
     printf("\n");
 }
@@ -37,6 +36,6 @@ int main(void) {
         {4, 9, 12, 14},
         {10, 11, 15, 16},
     };
-    PrintMatrix(N, &matrix[0][0]);
+    PrintMatrix(N, N, &matrix[0][0]);
     return 0;
 }
