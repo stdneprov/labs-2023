f24d2966b739e21bc32560c96ae2f336ec5d6f13
evaki53 <143840299+evaki53@users.noreply.github.com>
2023-12-09 16:54:51 +0300

    add scanf

--- lab14.c
+++ lab14.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-#define MAX_N 4
+#define MAX_N 7
 
 
 int main() {
     int matrix[MAX_N][MAX_N];
-    for (int i = 0; i < MAX_N; i++) {
-        for (int j = 0; j < MAX_N; j++) {
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             scanf("%d", &matrix[i][j]);
         }
     }
-    int row_Start = MAX_N;
+    int row_Start = n;
     int row_End = 0;
     int col_Start = 0;
-    int col_End = MAX_N;
+    int col_End = n;
     while (row_Start > row_End && col_Start < col_End) {
         for (int i = row_Start - 1; i >= row_End; i--) {
             printf("%d ", matrix[col_Start][i]);
