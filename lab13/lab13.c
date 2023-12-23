54346c8474ed7ab42329a110418f40e50e209d43
Shah29py <gebeevs@gmail.com>
2023-12-23 12:54:45 +0300

    laba13

--- lab13/lab13.c
+++ lab13/lab13.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SOGL11 66567390  // множество согласных
+#define SOGL11 66567390
 
 
 int IsSeparator(char c) {
     return (c == ' ') || (c == ',') || (c == '\n') || (c == '\t') || (c == EOF);
 }
 
-int CharBytes(int twin) { //считает кол-во байт
+int CharBytes(int twin) {
     if (twin >> 7 == 0) {
         return 1;
     } else if ((twin >> 5 == 6)) {
@@ -20,23 +20,23 @@ int CharBytes(int twin) { //считает кол-во байт
 
 typedef long long Set;
 
-void AddLittle(Set *a, char wer) { //добавляет в множество маленькие буквы
+void AddLittle(Set *a, char wer) {
     *a |= 1 << (wer - 1071);
 }
 
-void AddBig(Set *a, char wer) { //добавляет в множество большие буквы
+void AddBig(Set *a, char wer) {
     *a |= 1 << (wer - 1039);
 }
 
 int main() {
     int n, result = 0;
-    int result1 = 0; //результат для одного слова (1 значит, что в слове только 1 согласная)
-    Set a = 0; //множество глассных букв нынешнего слова
+    int result1 = 0;
+    Set a = 0; 
     while(1) {
         int c = getchar();
         
         if (IsSeparator(c)) {
-            if (result1 == 1) { // сравнивает множества
+            if (result1 == 1) { 
                 result = 1;
             }
             result1 = 0;
@@ -44,17 +44,17 @@ int main() {
                 continue;
             }
         }
-        if (c == EOF) { //заканчивает программу
+        if (c == EOF) { 
             break;
         }
-        if (CharBytes(c) == 1) { //считывает все байты одной буквы
+        if (CharBytes(c) == 1) { 
             continue;
         }
         if (CharBytes(c) == 2) {
             int rus = getchar();
-            int m = ((c & 31) << 6) | (rus & 63); //преобразует в юникод
+            int m = ((c & 31) << 6) | (rus & 63);
             if ((((1 << (m - 1072)) & SOGL11) != 0) || (((1 << (m - 1040)) & SOGL11) != 0)) { 
-                if (m > 1040 && m < 1072) { //проверяет на то, большая ли это буква
+                if (m > 1040 && m < 1072) { 
                     AddBig(&a, m);
                     result1 += 1;
                 } else {
@@ -65,12 +65,12 @@ int main() {
 
 
         }
-        if (CharBytes(c) == 3) { //считывает все байты одной буквы
+        if (CharBytes(c) == 3) {
             int c2 = getchar();
             int c3 = getchar();
             continue;
         }
-        if (CharBytes(c) == 4) { //считывает все байты одной буквы
+        if (CharBytes(c) == 4) {
             int c2 = getchar();
             int c3 = getchar();
             int c4 = getchar();
