1b3e3a5c627338e6dbbda5b27a32fb97b51a3172
Shah29py <143871963+Shah29py@users.noreply.github.com>
2023-12-23 15:01:15 +0300

    labs15

--- lab15.c
+++ lab15.c
@@ -14,19 +14,28 @@ int main() {
             } 
         } 
 
-        int sum,z, d; 
+        int sum, z, d; 
         for (i=0; i < n; i++) { 
             sum=mat[i][i]; 
             j = i; 
             int s = i,p = j; 
-            while ((s - 1) >= 0 && (p + 1) < n){s -= 1;p += 1;sum += mat[s][p];} 
+            while ((s - 1) >= 0 && (p + 1) < n) {
+                s -= 1;
+                p += 1;
+                sum += mat[s][p];
+            } 
             s = i;p = j; 
-            while ((s + 1) < n && (p - 1) >= 0){s += 1;p -= 1;sum += mat[s][p];} 
-            if (sum != 0){mat[i][j] = sum;} 
+            while ((s + 1) < n && (p - 1) >= 0) {
+                s += 1;
+                p -= 1;
+                sum += mat[s][p];} 
+                if (sum != 0) {
+                    mat[i][j] = sum;
+                } 
         } 
 
         for (z = 0; z < n; z++){ 
-            for (d = 0; d < n; d++){ 
+            for (d = 0; d < n; d++) { 
                 printf("%d ",mat[z][d]); 
             } 
             printf("\n"); 
