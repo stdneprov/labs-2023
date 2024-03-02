b1e2b280db3f35f5192bbb2877010639cc12a652
nPaBwaYT <surge.anton@gmail.com>
2024-03-02 22:57:14 +0300

    теперь свой вариант

--- mycode.c
+++ mycode.c
@@ -4,8 +4,8 @@
 typedef struct Node Node;
 
 struct Node {
-    Node* son;
-    Node* bro;
+    Node* left;
+    Node* right;
 
     int value;
     int depth;
@@ -23,51 +23,108 @@ void Init(Node** ptr, int val) {
     }
     (*ptr)->value = val;
     (*ptr)->depth = 0;
-    (*ptr)->bro = NULL;
-    (*ptr)->son = NULL;
+    (*ptr)->left = NULL;
+    (*ptr)->right = NULL;
+    printf("Ура, можно работать\n");
 }
 
-void Add(Node* parent, int val) {
+int Add(Node** parent, int val) {
     Node* ptr = (Node*)malloc(sizeof(Node));
+    int d = 0;
     if (ptr == NULL) {
         printf("Узел не добавлен, недостаточно памяти\n");
-        return;
+        return 0;
     }
 
-    if (parent->son == NULL) {
-        parent->son = ptr;
-    } else {
-        Node* olderBro = parent->son;
-        while (olderBro->bro != NULL) {
-            olderBro = olderBro->bro;
+    while (*parent != NULL) {
+        if (val > (*parent)->value) {
+            parent = &((*parent)->right);
+            ++d;
+        } else if (val < (*parent)->value) {
+            parent = &((*parent)->left);
+            ++d;
+        } else {
+            printf("Узел с таким значением уже существует\n");
+            return 0;
         }
-        olderBro->bro = ptr;
-    }
+    } 
+    *parent = ptr;
     ptr->value = val;
-    ptr->depth = parent->depth + 1;
-    ptr->bro = NULL;
-    ptr->son = NULL;
+    ptr->depth = d;
+    ptr->left = NULL;
+    ptr->right = NULL;
+    return 1;
 }
 
-void Clear(Node** targ) {
-    if ((*targ)->son != NULL) {
-        Clear(&((*targ)->son));
+void Move(Node* targ) {
+    if (targ == NULL) {
+        return;
     }
-    if ((*targ)->bro != NULL) {
-        Clear(&((*targ)->bro));
-    }
-    free(*targ);
-    *targ = NULL;
+    Move(targ->left);
+    Move(targ->right);
+    --(targ->depth);
 }
 
 void Del(Node** targ) {
-    if ((*targ)->son != NULL) {
-        Clear(&((*targ)->son));
-    }
+    Node** ptr;
+    Node* new;
 
-    Node* youngerBro = (*targ)->bro;
-    free(*targ);
-    *targ = youngerBro;
+    if ((*targ)->right != NULL) {
+        ptr = &((*targ)->right);
+        while ((*ptr)->left != NULL) {
+            ptr = &(*ptr)->left;
+        }
+        new = *ptr;
+
+        if ((*ptr)->right != NULL) {
+            *ptr = (*ptr)->right;
+            Move(*ptr);
+        } else {
+            *ptr = NULL;
+        }
+
+        new->right = (*targ)->right;
+        new->left = (*targ)->left;
+        new->depth = (*targ)->depth;
+
+        free(*targ);
+        *targ = new;
+
+    } else if ((*targ)->left != NULL) {
+        ptr = &((*targ)->left);
+        while ((*ptr)->right != NULL) {
+            ptr = &(*ptr)->right;
+        }
+        new = *ptr;
+
+        if ((*ptr)->left != NULL) {
+            *ptr = (*ptr)->left;
+            Move(*ptr);
+        } else {
+            *ptr = NULL;
+        }
+
+        new->left = (*targ)->left;
+        new->right = (*targ)->right;
+        new->depth = (*targ)->depth;
+
+        free(*targ);
+        *targ = new;
+
+    } else {
+        free(*targ);
+        *targ = NULL;
+    }
+}
+
+void Clear(Node** tree) {
+    if (*tree == NULL) {
+        return;
+    }
+    Clear(&((*tree)->left));
+    Clear(&((*tree)->right));
+    free(*tree);
+    *tree = NULL;
 }
 
 void PrintTree(Node* ptr) {
@@ -76,35 +133,48 @@ void PrintTree(Node* ptr) {
     }
     printf("%d\n", ptr->value);
 
-    if (ptr->son != NULL) {
-        PrintTree(ptr->son);
+    if (ptr->right != NULL) {
+        PrintTree(ptr->right);
     }
 
-    if (ptr->bro != NULL) {
-        PrintTree(ptr->bro);
+    if (ptr->left != NULL) {
+        PrintTree(ptr->left);
     }
 }
 
-int MyFunc(Node* parent) {
-    int count = 0;
-    int num = 0;
-    Node* ptr = parent->son;
-    while (ptr != NULL) {
-        ++count;
-        num += MyFunc(ptr);
-        ptr = ptr->bro;
+int _myFunc(Node* tree, int* d, int* v) {
+    if (tree == NULL) {
+        return 0;
+
+    } else if ((tree->right != NULL) || (tree->left != NULL)) {
+
+        _myFunc(tree->left, d, v);
+        _myFunc(tree->right, d, v);
+        if (tree->depth > *d) {
+            *d = tree->depth;
+            *v = tree->value;
+        }
+        return 1;
+    } else {
+        return 0;
     }
-    if (count == parent->value) {
-        ++num;
+}
+
+void MyFunc(Node* tree) {
+    int d = tree->depth;
+    int v = tree->value;
+    if (_myFunc(tree, &d, &v)) {
+        printf("Значение самой глубокой нетерминальной вершины: %d\n", v);
+    } else {
+        printf("Нетерминальной вершины не существует\n");
     }
-    return num;
 }
 
 void OnCheck(Node** tree) {
     printf("\n_____________________________________________\n\ni [value] - инициализация начального узла значением value\n\n");
-    printf("a [value] [son number 1] ... [son number n] - добавить узел, родителем которого будет i-й сын j-го\n сына и тд. Индексация с нуля\n\n");
-    printf("d [son number 1] ... [son number n] - удалить узел , выбор узла аналогичен добавлению\n\n");
-    printf("f [val] - функция, согласно варианту\n\n");
+    printf("a [value] - добавить узел\n\n");
+    printf("d [0..1] ... [0..1] - удалить узел, 0 - левый сын, 1 - правый. Отсутствие параметров - удаление\n первого узла\n\n");
+    printf("f - функция, согласно варианту\n\n");
     printf("p - вывести графическую визуализацию дерева\n\n");
     printf("s - очистка памяти и завершение работы программы\n\nПрошу не ставить лишние пробелы в конце строк\n_____________________________________________\n\n");
     
@@ -116,7 +186,7 @@ void OnCheck(Node** tree) {
         
         command = getchar();
 
-        if (*tree == NULL && command != 'i') {
+        if (*tree == NULL && (command != 'i' && command != 's')) {
             printf("Ты тупой или чё?\n");
             while (getchar() != '\n'){
                 continue;
@@ -133,44 +203,21 @@ void OnCheck(Node** tree) {
                 while (getchar() != '\n'){
                     continue;
                 }
-                printf("Ура, можно работать\n");
                 break;
 
             case 'a':
                 getchar();
-                Node* ptrA = *tree;
                 int val;
                 scanf("%d", &val);
-                int targA = -1;
-                    
-                while (getchar() != '\n') {
-                    scanf("%d", &targA);
-                    
-                    if (targA == -1) {
-                        break;
-                    }
-                    ptrA = ptrA->son;
-                    
-                    for (int i = 0; i < targA; ++i) {
-                        if (ptrA == NULL) {
-                            break;
-                        }
-                        ptrA = ptrA->bro;
-                    }
-                    if (ptrA == NULL) {
-                        break;
-                    }
+                
+                if (Add(tree, val)) {
+                    printf("Узел добавлен\n");
                 }
                 
-                if (ptrA != NULL) {
-                    Add(ptrA, val);
-                    printf("Узел добавлен\n");
-                } else {
-                    printf("Не существует узла в этой позиции\n");
-                    while (getchar() != '\n'){
+                while (getchar() != '\n'){
                     continue;
-                    }
                 }
+                
                 break;
 
             case 'd':
@@ -183,14 +230,13 @@ void OnCheck(Node** tree) {
                     if (targD == -1) {
                         break;
                     }
-                    ptrD = &((*ptrD)->son);
-                    
-                    for (int i = 0; i < targD; ++i) {
-                        if (*ptrD == NULL) {
-                            break;
-                        }
-                        ptrD = &((*ptrD)->bro);
+
+                    if (targD == 1) {
+                        ptrD = &((*ptrD)->right);
+                    } else if (targD == 0) {
+                        ptrD = &((*ptrD)->left);
                     }
+        
                     if (*ptrD == NULL) {
                         break;
                     }
@@ -199,10 +245,14 @@ void OnCheck(Node** tree) {
                 if (*ptrD != NULL) {
                     Del(ptrD);
                     printf("Узел удален\n");
+                    if (*tree == NULL) {
+                        command = 's';
+                        printf("Я тебе больше не нужен? Ну лан... Тогда сдохну :(\n");
+                    }
                 } else {
                     printf("Не существует узла в этой позиции\n");
                     while (getchar() != '\n'){
-                    continue;
+                        continue;
                     }
                 }
                 break;
@@ -211,7 +261,7 @@ void OnCheck(Node** tree) {
                 while (getchar() != '\n'){
                     continue;
                 }
-                printf("Количество узлов, степень которых совпадает со значением: %d\n", MyFunc(*tree));
+                MyFunc(*tree);
                 break;
             
             case 'p':
@@ -228,6 +278,9 @@ void OnCheck(Node** tree) {
                 }
                 if (*tree != NULL) {
                     Clear(tree);
+                } else {
+                    printf("Ну ок\n");
+                    break;
                 }
                 printf("Я тебе больше не нужен? Ну лан... Тогда сдохну :(\n");
                 break;
