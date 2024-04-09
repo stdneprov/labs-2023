e959fa47bd9f617025953001f36023ba0cb18077
CHUVISS <chuvilova2005@gmail.com>
2024-04-09 16:30:48 +0300

    Chuvilov 25-26

---
+++ lab25-26/PRC/lab26.c
@@ -0,0 +1,53 @@
+#include "lab26.h"
+
+int main() {
+    Stack stack;
+    Init(&stack);
+
+    int choice, value;
+
+    while (1) {
+        printf("\nВыберите операцию:\n");
+        printf("1. Добавить элемент в стек\n");
+        printf("2. Удалить элемент из стека\n");
+        printf("3. Получить верхний элемент стека\n");
+        printf("4. Вывести содержимое стека\n");
+        printf("5. Выход\n");
+        printf("6. Сортировка линейным выбором, с последующим удалением максимального элемента\n");
+        printf("Введите ваш выбор: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Введите значение для добавления: ");
+                scanf("%d", &value);
+                Push(&stack, value);
+                break;
+            case 2:
+                printf("Извлеченный элемент: %d\n", Pop(&stack));
+                break;
+            case 3:
+                if (StackTop(&stack) == 0){
+                    printf("Ошибка: стек пуст");
+                }
+                else{
+                    printf("Верхний элемент стека: %d\n", StackTop(&stack));
+                }
+                break;
+            case 4:
+                StackPrint(&stack);
+                break;
+            case 5:
+                printf("Выход из программы.\n");
+                exit(0);
+            case 6:
+                BubbleSort(&stack);
+                StackPrint(&stack);
+                break;
+            default:
+                printf("Неверный выбор. Попробуйте снова.\n");
+                exit(0);
+        }
+    }
+    return 0;
+}
---
+++ lab25-26/PRC/lab26.h
@@ -0,0 +1,100 @@
+#ifndef Stack_h
+#define Stack_h
+#include <stdio.h>
+#include <stdlib.h>
+
+// Структура элемента стека
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+// Структура стека
+typedef struct {
+    Node* top;
+} Stack;
+
+// Инициализация стека
+void Init(Stack* stack) {
+    stack->top = NULL;
+}
+
+// Проверка, пуст ли стек
+int IsEmpty(Stack* stack) {
+    return stack->top == NULL;
+}
+
+// Добавление элемента в стек
+void Push(Stack* stack, int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Ошибка: память для нового узла не выделена\n");
+        return;
+    }
+    newNode->data = data;
+    newNode->next = stack->top;
+    stack->top = newNode;
+}
+
+// Удаление элемента из стека
+int Pop(Stack* stack) {
+    if (IsEmpty(stack)) {
+        printf("Ошибка: стек пуст\n");
+        return -1;
+    }
+    Node* temp = stack->top;
+    int data = temp->data;
+    stack->top = temp->next;
+    free(temp);
+    return data;
+}
+
+// Получение верхнего элемента стека без его удаления
+int StackTop(Stack* stack) {
+    if (IsEmpty(stack)) {
+        return 0;
+    }
+    return stack->top->data;
+}
+
+// Функция для вывода содержимого стека
+void StackPrint(Stack *stk) {
+    if (IsEmpty(stk)) {
+        printf("Стек пуст\n");
+    }
+    printf("Элементы стека: ");
+    for (Node *i = stk->top; i != NULL; i = i->next) {
+        printf("%d", i->data);
+        if (i->next != NULL) {
+            printf(" -> ");
+        }
+    }
+    printf("\n");
+}
+
+void BubbleSort(Stack *stack) {
+    Stack tempStack;
+    //initialize(&tempStack);
+
+    while (!IsEmpty(stack)) {
+        int current = Pop(stack);
+
+        // Переносим элементы из временного стека обратно в основной стек,
+        // если они больше текущего элемента
+        while (!IsEmpty(&tempStack) && StackTop(&tempStack) < current) {
+            Push(stack, Pop(&tempStack));
+        }
+
+        Push(&tempStack, current);
+    }
+
+    // Переносим элементы из временного стека обратно в основной стек
+    while (!IsEmpty(&tempStack)) {
+        Push(stack, Pop(&tempStack));
+    }
+
+    Pop(stack);
+
+}
+
+#endif
