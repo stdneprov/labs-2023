d834d27dc32bb3d128a4223e1ba709113cb7f7ad
abergine <143881945+abergine@users.noreply.github.com>
2024-04-10 04:30:02 +0300

    upd

--- bubble_sort.c
+++ bubble_sort.c
@@ -5,7 +5,6 @@ void BubbleSort(udt *q) {
     int size = udt_size(q);
     for (int i = 0; i < size - 1; i++) {
         for (int j = 0; j < size - i - 1; j++) {
-            // Swap adjacent elements if they are in the wrong order
             QueueItem *current = q->head;
             for (int k = 0; k < j; k++) {
                 current = current->next;
@@ -13,7 +12,6 @@ void BubbleSort(udt *q) {
             int val1 = current->value;
             int val2 = current->next->value;
             if (val1 > val2) {
-                // Swap values
                 int temp = val1;
                 current->value = val2;
                 current->next->value = temp;
