e48d2f05578ddd9ee6a78806057b78dc5b195d6b
abergine <143881945+abergine@users.noreply.github.com>
2024-04-16 00:01:19 +0300

    fixed bubble_sort.c
    now only using queue functions

--- bubble_sort.c
+++ bubble_sort.c
@@ -2,20 +2,21 @@
 #include "bubble_sort.h"
 
 void BubbleSort(udt *q) {
-    int size = udt_size(q);
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            QueueItem *current = q->head;
-            for (int k = 0; k < j; k++) {
-                current = current->next;
-            }
-            int val1 = current->value;
-            int val2 = current->next->value;
-            if (val1 > val2) {
-                int temp = val1;
-                current->value = val2;
-                current->next->value = temp;
+    T size = udt_size(q);
+    bool isInOrder = false;
+    while (isInOrder == false) {
+        isInOrder = true;
+        T first = udt_pop(q);
+        for (int i = 0; i < size - 1; i++) {
+            T temp = udt_pop(q);
+            if (temp < first) {
+                udt_push(q, first);
+                first = temp;
+            } else {
+                udt_push(q, temp);
+                isInOrder = false;
             }
         }
+        udt_push(q, first);
     }
 }
