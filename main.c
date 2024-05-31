12fb3493ff2d8cafbb141c641c51dcb626b109a7
FortuneHunter78 <92946832+FortuneHunter78@users.noreply.github.com>
2024-05-31 14:25:50 +0300

    KP_9

---
+++ main.c
@@ -0,0 +1,92 @@
+#include "table.h"
+#include "sort.h"
+
+
+int main() {
+    FILE* d = fopen("data.txt", "r");
+    FILE* k = fopen("keys.txt", "r");
+    if(!d || !k)
+        exit(-1);
+    Table* table = CreateTable();
+    if(!table) {
+        Destroy(table);
+        return 0;
+    }
+
+    item nr;
+    while (fscanf(d, "%s", nr.val) > 0) {
+        float digits;
+        fscanf(k, "%f", &digits);
+        nr.val[strcspn(nr.val, "\n")] = 0;
+        nr.key = digits;
+        Add(table, nr);
+        nr.key = 0;
+    }
+
+
+    fclose(d);
+    fclose(k);
+    if(!table) {
+        exit(-1);
+    }
+
+    printf("\nTable\n");
+    Print(table);
+    char input[20];
+    char printTable[10] = "print";
+    char sortTable[15] = "sort";
+    char exitMenu[10] = "exit";
+    char search[10] = "search";
+    char command[10] = "";
+
+    while(strcmp(command, exitMenu) != 0) {
+        printf("\n");
+        printf("---------------------------------------------------------\n");
+        printf("sort - sort table\n");
+        printf("exit - exit programm\n");
+        printf("search KEY - search element for key in sorted table\n");
+        printf("print - print table\n");
+        printf("---------------------------------------------------------\n");
+
+        char command[15] = "";
+        fgets(input, sizeof(char) * 20, stdin);
+        if (feof(stdin)) {
+            break;
+        }
+        float num;
+        int ind = 0;
+        char forNum[20] = "";
+        for (int i = 0; input[i] != '\n'; ++i) {
+            if (input[i] != ' ' && input[i] !='\n' && input[i] > '9') {
+                command[i] = input[i];
+                input[i] = ' ';
+            }
+            else {
+                if (input[i] != ' ' && input[i] !='\n') {
+                    forNum[ind] = input[i];
+                    ind++;
+                }
+            }
+        }
+        sscanf(forNum, "%f", &num);
+
+        if (strcmp(command, printTable) == 0) {
+            Print(table);
+        } else if (strcmp(command, sortTable) == 0) {
+            QuickSort(table);
+            Print(table);
+        } else if (strcmp(command, search) == 0) {
+            int res = Search(table, num);
+            if (res == -1) {
+                printf("Not found\n");
+            } else {
+                printf("[%f] -> (%s)\n", num, table->rows[res].val);
+            }
+        } else if (strcmp(command, exitMenu) == 0) {
+            Destroy(table);
+            break;
+        } else {
+            printf("-Wrong command\n");
+        }
+    }
+}
---
+++ table.c
@@ -0,0 +1,76 @@
+#include "table.h"
+#include "sort.h"
+
+
+
+Table* CreateTable() {
+    Table* tb = (Table*)malloc(sizeof(Table));
+    if(!tb) {
+        return NULL;
+    } 
+    item* data = (item*)malloc(MIN_CAPACITY * sizeof(item));
+    if(!data) {
+        free(tb);
+        return NULL;
+    }
+    tb->capacity = MIN_CAPACITY;
+    tb->rows = data;
+    tb->size = 0;
+    return tb;
+}
+
+bool Buffer(Table* t) {
+    if(t->size < t->capacity)
+        return true;
+    int new_cap = 2 * t->capacity;
+    item* tmp = (item*)realloc(t->rows, sizeof(item) * new_cap);
+    if(!tmp) {
+        return false; 
+    }
+    t->capacity = new_cap;
+    t->rows = tmp;
+    return true;
+}
+
+bool Add(Table* t, item r) {
+    if(!Buffer(t))
+        return false; 
+    t->rows[t->size] = r;
+    t->size++;
+    return true;
+}
+
+void Print(Table* t) {
+    for(int i = 0; i < t->size; ++i)
+        printf("%2d | %.3f | %s\n", i, t->rows[i].key, t->rows[i].val);
+}
+
+void Destroy(Table* t) {
+    free(t->rows);
+    free(t);
+}
+
+int Search(Table* t, float k) {
+    int l = 0;
+    int r = t->size - 1;
+    int result = -1;
+    while(l <= r) {
+        int x = (l + r) >> 1;
+        int c;
+        if (t->rows[x].key == k) {
+            c = 0;
+        } else if (t->rows[x].key > k) {
+            c = 1;
+        } else {
+            c = -1;
+        }
+        if(c == 0) {
+            result = x;
+            r = x - 1;
+        } else if(c > 0)
+            r = x - 1;
+        else 
+            l = x + 1;
+    }
+    return result;
+}
